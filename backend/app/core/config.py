c50048f61f640faa9b9ca2f4313ff65cf61813cd
KiralyAlparCsaba <kiraly.alpar.csaba@student.ms.sapientia.ro>
2026-05-07 12:52:02 +0300

    model adjustments

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -13,7 +13,7 @@ class Settings(BaseSettings):
     
     # Model Settings
     models_directory: str = "static/models"
-    default_model_filename: str = "sapi3D_V1.6.glb"
+    default_model_filename: str = "sapi3D_V1.6.1.glb"
 
     # Avatar Upload Settings
     avatars_directory: str = "static/avatars"
